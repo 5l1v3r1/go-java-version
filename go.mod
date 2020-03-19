0eb8e8ccea0528764593fcc1c86df50c38ed3a8a
masahiro331 <m_fujimura@r.recruit.co.jp>
2020-03-19 16:50:33 +0900

    First init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/masahiro331/go-java-version
+
+go 1.13
+
+require github.com/dglo/java2go v0.0.0-20140814220312-00603a7cc6c3 // indirect
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/*
+1. split "-", "." separator
+2. parseInt
+3. parseString special charactor "ga", "final", "alpha", etc...
+4. compare!!!
+*/
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+type Version string
+
+func Parse(s string) Version {
+	return
+}
